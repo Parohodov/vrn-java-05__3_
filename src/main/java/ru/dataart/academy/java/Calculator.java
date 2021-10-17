a8547f97a73a4a1f6d8fb7e59b517ee6a3957578
Pavel <82582511+Parohodov@users.noreply.github.com>
2021-10-17 22:43:38 +0300

    Get sum done

--- src/main/java/ru/dataart/academy/java/Calculator.java
+++ src/main/java/ru/dataart/academy/java/Calculator.java
@@ -1,7 +1,9 @@
 package ru.dataart.academy.java;
 
-import java.util.Collections;
-import java.util.List;
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.iterators.ReverseListIterator;
+
+import java.util.*;
 
 public class Calculator {
     /**
@@ -11,7 +13,19 @@ public class Calculator {
      */
     public Integer getSum(List<Integer> firstNumber, List<Integer> secondNumber) {
         //Task implementation
-        return 0;
+
+        int result = 0;
+
+        // Using Collections.reverse() or Guava's Lists.reverse() and Iterator<T>
+        result = getSumWithIterator(firstNumber, secondNumber);
+
+        // Using Java Collections Framework ListIterator<T>
+        result = getSumWithListIterator(firstNumber, secondNumber);
+
+        // Using Apache's commons-collections4 ReverseListIterator
+        result = getSumWithReversedIterator(firstNumber, secondNumber);
+
+        return result;
     }
 
     /**
@@ -38,4 +52,77 @@ public class Calculator {
         // Task implementation
         return Collections.EMPTY_LIST;
     }
+
+    //==================================================================================================================
+
+    public int getSumWithIterator(List<Integer> firstNumber, List<Integer> secondNumber) {
+        int num1 = 0;
+        int num2 = 0;
+
+        // A basic Iterator only moves forward, so the collections are needed to be reversed
+        // The problem is that it's necessary to create new collections
+
+        // Using Collections.reverse()
+//        List<Integer> firstReversed = new ArrayList<>(firstNumber);
+//        Collections.reverse(firstReversed);
+//
+//        List<Integer> secondReversed = new ArrayList<>(secondNumber);
+//        Collections.reverse(secondReversed);
+
+        // Using Guava's Lists.reverse()
+        Iterator<Integer> iter = Lists.reverse(firstNumber).iterator();
+        while(iter.hasNext()) {
+            num1 = num1*10 + iter.next();
+        }
+        iter = Lists.reverse(secondNumber).iterator();
+        while (iter.hasNext()) {
+            num2 = num2*10 + iter.next();
+        }
+
+        // I like to use Idea's "iter" autocompletion, but it's not representative in terms of taking lessons
+//        for (Integer integer : firstList) {
+//            num1 = num1*10 + integer;
+//        }
+//        for (Integer integer : secondList) {
+//            num2 = num2*10 + integer;
+//        }
+        return num1 + num2;
+    }
+
+    public int getSumWithListIterator(List<Integer> firstNumber, List<Integer> secondNumber) {
+        int num1 = 0;
+        int num2 = 0;
+
+        // Fortunately a ListIterator can move backward that is very useful in this case
+        // and this code can work with more abstract List<E> type
+        ListIterator<Integer> liter = firstNumber.listIterator(firstNumber.size());
+        while(liter.hasPrevious()) {
+            num1 = num1*10 + liter.previous();
+        }
+
+        liter = secondNumber.listIterator(secondNumber.size());
+        while(liter.hasPrevious()) {
+            num2 = num2*10 + liter.previous();
+        }
+
+        return num1 + num2;
+    }
+
+    private int getSumWithReversedIterator(List<Integer> firstNumber, List<Integer> secondNumber) {
+        int num1 = 0;
+        int num2 = 0;
+
+        ReverseListIterator<Integer> reverseIter = new ReverseListIterator<>(firstNumber);
+        while(reverseIter.hasNext()) {
+            num1 = num1*10 + reverseIter.next();
+        }
+
+        reverseIter = new ReverseListIterator<>(secondNumber);
+        while(reverseIter.hasNext()) {
+            num2 = num2*10 + reverseIter.next();
+        }
+
+        return num1 + num2;
+    }
+
 }
